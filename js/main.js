7b1140a538b23f037aba03265b567439900aabba
lahiru-alavi <lahirua@alavi.lk>
2021-05-21 22:15:25 +0530

    ui-fix-v1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,45 @@ $(function () {
   });
 });
 // back to top
+
+// Progress Bar
+var bar1 = new ldBar("#w-rev", {
+  value: 90,
+  preset: "line",
+});
+
+var bar2 = new ldBar("#w-spe", {
+  value: 90,
+  preset: "line",
+});
+
+var bar3 = new ldBar("#f-rev", {
+  value: 10,
+  preset: "line",
+});
+
+var bar4 = new ldBar("#f-spe", {
+  value: 10,
+  preset: "line",
+});
+
+var bar5 = new ldBar("#f-roa", {
+  value: 10,
+  preset: "line",
+});
+
+var bar6 = new ldBar("#g-rev", {
+  value: 10,
+  preset: "line",
+});
+
+var bar7 = new ldBar("#g-spe", {
+  value: 10,
+  preset: "line",
+});
+
+var bar8 = new ldBar("#g-roa", {
+  value: 10,
+  preset: "line",
+});
+// Progress Bar
